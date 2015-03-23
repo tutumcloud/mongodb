0a08bca1e4a53be2663e345e4d6233390f25e4bc
Yves-Marie Saout <chatov42@gmail.com>
2015-03-22 20:06:45 -0400

    Fix admin permissions

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -19,7 +19,7 @@ while [[ RET -ne 0 ]]; do
 done
 
 echo "=> Creating an admin user with a ${_word} password in MongoDB"
-mongo admin --eval "db.addUser({user: 'admin', pwd: '$PASS', roles: [ 'userAdminAnyDatabase', 'dbAdminAnyDatabase' ]});"
+mongo admin --eval "db.addUser({user: 'admin', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
 mongo admin --eval "db.shutdownServer();"
 
 echo "=> Done!"
