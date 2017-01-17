e449bf9cbda6fc8686dbb6b72b8a2546018eebc2
richersoon <richersoon@gmail.com>
2017-01-17 22:03:21 +0800

    Added mongo version 3.4

---
+++ 3.4/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -m
+
+mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
+cmd="$mongodb_cmd --httpinterface --rest --master"
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
+
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+$cmd &
+
+if [ ! -f /data/db/.mongodb_password_set ]; then
+    /set_mongodb_password.sh
+fi
+
+fg
